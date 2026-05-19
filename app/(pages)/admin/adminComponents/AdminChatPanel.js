b122417b8a4573aea3e0d63a2a9cd3be693dd22e
Rian Hasan Siam <rianhasan1971@gmail.com>
2026-05-20 01:30:55 +0600

    fixed deployment

--- app/(pages)/admin/adminComponents/AdminChatPanel.js
+++ app/(pages)/admin/adminComponents/AdminChatPanel.js
@@ -148,7 +148,7 @@ export default function AdminChatPanel() {
         }
       };
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+
   }, [session]);
 
   const fetchConversations = async () => {
